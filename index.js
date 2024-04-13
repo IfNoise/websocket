3bfe9267b157a8958d478a904cf3d325ed0fb974
IfNoise <black_russan@mail.ru>
2024-04-13 15:41:46 +0700

    новый файл:    .gitignor 	новый файл:    index.js 	новый файл:    package-lock.json 	новый файл:    package.json

---
+++ index.js
@@ -0,0 +1,21 @@
+import { createServer } from 'http';
+import { WebSocketServer } from 'ws';
+
+const server = createServer();
+const wss = new WebSocketServer({ server });
+
+wss.on('connection', function connection(ws, req) {
+
+  console.log('connected');
+  console.log('request', req);
+  
+  ws.on('error', console.error);
+
+  ws.on('message', function message(data) {
+    console.log('received: %s', data);
+  });
+
+  ws.send('something');
+});
+
+server.listen(8080);
