429c027eccfcae65cae4a09c27507c5cfa580214
IfNoise <black_russan@mail.ru>
2024-04-14 11:24:06 +0700

    изменено:      index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const wss = new WebSocketServer({ server });
 wss.on('connection', function connection(ws, req) {
 
   console.log('connected');
-  console.log('request', req);
+  console.log('request', req.headers.host);
   
   ws.on('error', console.error);
 
