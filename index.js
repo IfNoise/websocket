fdc81316f5a3820ce76f79b6b4ab191aa957361e
IfNoise <black_russan@mail.ru>
2024-04-17 19:18:01 +0700

    изменено:      index.js

--- index.js
+++ index.js
@@ -4,9 +4,18 @@ import mkrpc from './json-rpc-ws.js';
 const rpc = mkrpc();
 rpc.onopen = function() {
   console.log('Connected');
+  
+};
+setTimeout(function() {
   rpc.call({"id":68,"method":"Config.Get","params":{}}).then(function(result) {
     console.log('Result:', result);
   }).catch(function(err) {
     console.log('Error:', err);
   } )
-};
+  
+}, 5000);
+rpc.call({"id":68,"method":"Config.Get","params":{}}).then(function(result) {
+  console.log('Result:', result);
+}).catch(function(err) {
+  console.log('Error:', err);
+} )
