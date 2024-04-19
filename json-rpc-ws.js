898e00e4662165d2bc589a214c6b71deaa917426
IfNoise <black_russan@mail.ru>
2024-04-19 21:01:03 +0700

    изменено:      json-rpc-ws.js

--- json-rpc-ws.js
+++ json-rpc-ws.js
@@ -1,16 +1,15 @@
 import { WebSocketServer } from "ws";
 
-
 class JSONRPCws {
-  constructor(port=8080, ondevice=null) {
+  constructor(port = 8080, ondevice = null) {
     this.wss = new WebSocketServer({ port }, () => {
       console.log("JSONRPCws listening ");
     });
-    this.ondevice = ondevice   
+    this.ondevice = ondevice;
     console.log("JSONRPCws created", this);
   }
-  devices=[];
-  start(){
+  devices = [];
+  start() {
     this.wss.on("connection", function connection(ws, req) {
       console.log("connected");
       const device = {
@@ -20,20 +19,9 @@ class JSONRPCws {
         status: "connected",
         address: req.socket.remoteAddress,
         calls: {},
-        call: function (method, params, timeoutMilli) {
-          return new Promise(function (resolve, reject) {
-            var frame = { id: this.id++, method, params };
-            // console.log('Sent:', JSON.stringify(frame));
-            this.calls[frame.id] = resolve;
-            this.ws.send(JSON.stringify(frame));
-            setTimeout(function () {
-              if (this.calls[frame.id]) {
-                reject("RPC call timeout" + JSON.stringify(frame));
-                delete this.calls[frame.id];
-              }
-            }, timeoutMilli || this.defaultTimeoutMs);
-          });
-        },
+        call: (method, params, timeoutMilli) => {
+          return this.call(device, method, params, timeoutMilli);
+        }
       };
       device.ws.on("message", function incoming(message) {
         console.log("Rcvd:", ev.data);
@@ -45,12 +33,30 @@ class JSONRPCws {
         device.status = "offline";
       });
       console.log("Device connected", device);
-      this.devices.push(device);
+      this.addDevice(device);
       if (this.ondevice) this.ondevice(device);
     });
   }
-  getDevices(){return this.devices};
-
+  call(device, method, params, timeoutMilli) {
+    return new Promise(function (resolve, reject) {
+      var frame = { id: device.id++, method, params };
+      // console.log('Sent:', JSON.stringify(frame));
+      device.calls[frame.id] = resolve;
+      device.ws.send(JSON.stringify(frame));
+      setTimeout(function () {
+        if (device.calls[frame.id]) {
+          reject("RPC call timeout" + JSON.stringify(frame));
+          delete device.calls[frame.id];
+        }
+      }, timeoutMilli || this.defaultTimeoutMs);
+    });
+  }
+  addDevice(device) {
+    this.devices.push(device);
+  }
+  getDevices() {
+    return this.devices;
+  }
 }
 
-export default JSONRPCws
+export default JSONRPCws;
