f8bdf75679417ab02cd813e53bcd7099125e2cb1
IfNoise <black_russan@mail.ru>
2024-04-19 16:16:25 +0700

    изменено:      index.js 	изменено:      json-rpc-ws.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { createServer } from 'http';
+/* import { createServer } from 'http';
 import { WebSocketServer } from 'ws';
 
 const server = createServer();
@@ -18,4 +18,15 @@ wss.on('connection', function connection(ws, req) {
   ws.send(JSON.stringify({"id":68,"method":"Config.Get","params":{}}));
 });
 
-server.listen(8080);
+server.listen(8080); */
+import JSONRPCws from "./json-rpc-ws";
+
+const jsonrpc = new JSONRPCws((device) => {
+  console.log("Device connected", device);
+  device.call("Config.Get", {}).then((result) => {
+    console.log("Config.Get result", result);
+  });
+}  
+);
+
+
--- json-rpc-ws.js
+++ json-rpc-ws.js
@@ -1,85 +1,49 @@
 import { WebSocketServer } from "ws";
 import WebSocket from "ws";
-import { createServer } from "http";
-/* 
-const mkrpc = function() {
-  var id = 1, reconn = true, calls = {}, defaultTimeoutMs = 3000, ws=null;
-  var engine = {
-    onopen: function() {},   // Called when WS connection is established
-    onclose: function() {},  // Called when WS connection is closed
-    onin: function() {},     // Called on each incoming frame
-    onout: function() {},    // Called on each outgoing frame
-    send: function(frame) {  // Send notification to the server
-      engine.onout(frame);
-      ws?.send(JSON.stringify(frame));
-    },
-    close: function() {  // Close and stop reconnecting
-      reconn = false;
-      ws.close();
-    },
-    call: function(method, params, timeoutMilli) {
-      return new Promise(function(resolve, reject) {
-        var frame = {id: id++, method, params};
-        // console.log('Sent:', JSON.stringify(frame));
-        calls[frame.id] = resolve;
-        engine.onout(frame);
-        ws?.send(JSON.stringify(frame));
-        setTimeout(function() {
-          if (calls[frame.id]) {
-            reject('RPC call timeout' + JSON.stringify(frame));
-            delete calls[frame.id];
-          }
-        }, timeoutMilli || defaultTimeoutMs);
+
+
+class JSONRPCws {
+  constructor(port=8080, ondevice=null) {
+    this.wss = new WebSocketServer({ port });
+    this.devices = [];
+    this.ondevice = null
+    this.wss.on("connection", function connection(ws, req) {
+      const device = {
+        ws: ws,
+        id: 1,
+        defaultTimeoutMs: 3000,
+        status: "connected",
+        address: req.headers["x-forwarded-for"],
+        calls: {},
+        call: function (method, params, timeoutMilli) {
+          return new Promise(function (resolve, reject) {
+            var frame = { id: this.id++, method, params };
+            // console.log('Sent:', JSON.stringify(frame));
+            this.calls[frame.id] = resolve;
+            this.ws.send(JSON.stringify(frame));
+            setTimeout(function () {
+              if (this.calls[frame.id]) {
+                reject("RPC call timeout" + JSON.stringify(frame));
+                delete this.calls[frame.id];
+              }
+            }, timeoutMilli || this.defaultTimeoutMs);
+          });
+        },
+      };
+      device.ws.on("message", function incoming(message) {
+        console.log("Rcvd:", ev.data);
+        var frame = JSON.parse(message);
+        if (this.calls[frame.id]) this.calls[frame.id](frame); // Resolve call promise
+        delete this.calls[frame.id];
       });
-    },
-  }; */
-/* 
-  var start = function() {
-    // console.log('Opening WS connection to', url);
-    const server = createServer();
-    const wss = new WebSocketServer({ server });
-    wss.on('connection', function connection(w, req) {
-    ws =w
-    console.log('Connected',req.headers);
-    ws.onopen = function(ev) {
-      
-      engine.onopen();
-    };
-    ws.onclose = function(ev) {
-      // console.log('Closed WS connection to', url);
-      engine.onclose();
-    };
-    ws.onerror = function(ev) {
-      console.log('WS connection error:', ev);
-    };
-    ws.onmessage = function(ev) {
-      console.log('Rcvd:', ev.data);
-      var frame = JSON.parse(ev.data);
-      engine.onin(frame);
-      if (calls[frame.id]) calls[frame.id](frame);  // Resolve call promise
-      delete calls[frame.id];
-    };
-  server.listen(8080);
-  })};
-  start();
-
-  return engine;
-}; */
-
-class JSONRPCws{
-  constructor(){
-    this.wss = new WebSocketServer({port:8080});
-    this.devices=[]
-    this.wss.on('connection',function connection(ws,req){
-      this.devices.push({ws:ws,status:'connected'})
-      ws.on('message',function incoming(message){})
-
-        
+      device.ws.on("close", () => {
+        device.status = "offline";
+      });
+      this.devices.push(device);
+      if (this.ondevice) this.ondevice(device);
     });
   }
-  call(method, params, timeoutMilli){
-
-  }
+  getDevices(){return this.devices};
 
 }
-export default mkrpc
+export default JSONRPCws;
