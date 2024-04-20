c5a5a5aad009baeb70b7b636cc56b27202f90c05
IfNoise <black_russan@mail.ru>
2024-04-20 08:51:50 +0700

    изменено:      index.js

--- index.js
+++ index.js
@@ -25,8 +25,9 @@ const jsonrpc = JSONRPCws(8080,(device) => {
   console.log("Device connected", device);
   device.call("Config.Get", {}).then((result) => {
     console.log("Config.Get result", result);
-    console.log( jsonrpc.getDevices())
+    
   });
+  console.log( jsonrpc.getDevices())
 }  
 );
 jsonrpc.start();
