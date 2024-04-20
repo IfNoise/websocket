5b6e7ec6ccf554bd89f373a46201f5656939bd4c
IfNoise <black_russan@mail.ru>
2024-04-20 08:43:49 +0700

    изменено:      index.js 	изменено:      json-rpc-ws.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ wss.on('connection', function connection(ws, req) {
 server.listen(8080); */
 import JSONRPCws from "./json-rpc-ws.js "
 
-const jsonrpc = new JSONRPCws(8080,(device) => {
+const jsonrpc = JSONRPCws(8080,(device) => {
   console.log("Device connected", device);
   device.call("Config.Get", {}).then((result) => {
     console.log("Config.Get result", result);
@@ -29,6 +29,5 @@ const jsonrpc = new JSONRPCws(8080,(device) => {
 }  
 );
 jsonrpc.start();
-
 
 
--- json-rpc-ws.js
+++ json-rpc-ws.js
@@ -1,62 +1,61 @@
 import { WebSocketServer } from "ws";
 
-class JSONRPCws {
-  constructor(port = 8080, ondevice = null) {
-    this.wss = new WebSocketServer({ port }, () => {
-      console.log("JSONRPCws listening ");
-    });
-    this.ondevice = ondevice;
-    console.log("JSONRPCws created", this);
-  }
-  devices = [];
-  start() {
-    this.wss.on("connection", function connection(ws, req) {
-      console.log("connected");
-      const device = {
-        ws: ws,
-        id: 1,
-        defaultTimeoutMs: 3000,
-        status: "connected",
-        address: req.socket.remoteAddress,
-        calls: {},
-        call: (method, params, timeoutMilli) => {
-          return this.call(device, method, params, timeoutMilli);
-        }
-      };
-      device.ws.on("message", function incoming(message) {
-        console.log("Rcvd:", ev.data);
-        var frame = JSON.parse(message);
-        if (this.calls[frame.id]) this.calls[frame.id](frame); // Resolve call promise
-        delete this.calls[frame.id];
+function JSONRPCws(port = 8080, ondevice = null) {
+  const devices = [];
+  const defaultTimeoutMs = 3000;
+  const wss = new WebSocketServer({ port }, () => {
+    console.log("JSONRPCws listening ");
+  });
+  const call = (device, method, params, timeoutMilli) => {
+      return new Promise(function (resolve, reject) {
+        var frame = { id: device.id++, method, params };
+        // console.log('Sent:', JSON.stringify(frame));
+        device.calls[frame.id] = resolve;
+        device.ws.send(JSON.stringify(frame));
+        setTimeout(function () {
+          if (device.calls[frame.id]) {
+            reject("RPC call timeout" + JSON.stringify(frame));
+            delete device.calls[frame.id];
+          }
+        }, timeoutMilli || defaultTimeoutMs);
       });
-      device.ws.on("close", () => {
-        device.status = "offline";
-      });
-      console.log("Device connected", device);
-      this.addDevice(device);
-      if (this.ondevice) this.ondevice(device);
-    });
-  }
-  call(device, method, params, timeoutMilli) {
-    return new Promise(function (resolve, reject) {
-      var frame = { id: device.id++, method, params };
-      // console.log('Sent:', JSON.stringify(frame));
-      device.calls[frame.id] = resolve;
-      device.ws.send(JSON.stringify(frame));
-      setTimeout(function () {
-        if (device.calls[frame.id]) {
-          reject("RPC call timeout" + JSON.stringify(frame));
-          delete device.calls[frame.id];
-        }
-      }, timeoutMilli || this.defaultTimeoutMs);
-    });
-  }
-  addDevice(device) {
-    this.devices.push(device);
-  }
-  getDevices() {
-    return this.devices;
-  }
+    };
+    const addDevice= (device) => {
+      devices.push(device);
+    };
+    const engine = {
+      start: () => {
+        wss.on("connection", function connection(ws, req) {
+          console.log("connected");
+          const device = {
+            ws: ws,
+            id: 1,
+            status: "connected",
+            address: req.socket.remoteAddress,
+            calls: {},
+            call: (method, params, timeoutMilli) => {
+              return call(device, method, params, timeoutMilli);
+            },
+          };
+          device.ws.on("message", function incoming(message) {
+            console.log("Rcvd:", ev.data);
+            var frame = JSON.parse(message);
+            if (device.calls[frame.id]) device.calls[frame.id](frame); // Resolve call promise
+            delete device.calls[frame.id];
+          });
+          device.ws.on("close", () => {
+            device.status = "offline";
+          });
+          console.log("Device connected", device);
+          addDevice(device);
+          if (this.ondevice) this.ondevice(device);
+        });
+      },
+      getDevices: () => {
+        return devices;
+      },
+    };
+  return engine;
 }
 
 export default JSONRPCws;
