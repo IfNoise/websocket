8f6bc0d976ba5b7de61557a4ae02a418fdc170bf
IfNoise <black_russan@mail.ru>
2024-04-28 07:44:47 +0700

    изменено:      index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.get("/devices", (req, res) => {
   );
 });
 app.get("/devices/:deviceId/getState", (req, res) => {
-  const { deviceId } = JSON.parse(req.params);
+  const { deviceId } =req.params;
   const device = jsonrpc.getDevices().filter((device) => device.deviceId === deviceId)[0];
   if (!device) {
     return res.status(404).send("No devices found");
