29f5181c701cfce0b2d441cd2cb5442a9a8275f1
IfNoise <black_russan@mail.ru>
2026-02-08 09:58:51 +0700

    feat: обновить конфигурацию сервера WebSocket и добавить создание директории для базы данных

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,18 @@
 module.exports = {
-  apps : [{
-    name   : "WebSocketServer",
-    script : "./index.js",
-     env_production: {
-       NODE_ENV: "production",
-       WS_PORT: 8080,
-       API_PORT: 3600
+  apps: [
+    {
+      name: "WebSocketServer_v2",
+      script: "./index.js",
+      env_production: {
+        NODE_ENV: "production",
+        WS_PORT: 8080,
+        API_PORT: 3600,
+      },
+      env_development: {
+        NODE_ENV: "development",
+        WS_PORT: 8082,
+        API_PORT: 3602,
+      },
     },
-    env_development: {
-       NODE_ENV: "development",
-       WS_PORT: 8080,
-       API_PORT: 3600
-    }
-  }]
-}
+  ],
+};
